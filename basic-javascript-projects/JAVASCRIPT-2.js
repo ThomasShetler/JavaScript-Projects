cc56e8a4e72f258d54f119c66da2ab88d2bc7303
ThomasShetler <tommyaasum22@gmail.com>
2019-10-17 15:53:53 -0700

    JavaScript2

--- basic-javascript-projects/JAVASCRIPT-2.js
+++ basic-javascript-projects/JAVASCRIPT-2.js
@@ -1,6 +1,14 @@
 var jojo = "hi";
 
 function changeColor(newColor) {
-    var element = document.getElementById('yum');
+    var element = document.getElementById('Concatenate');
     element.style.color = newColor;
+
+}
+
+function myFunction() {
+    var sentance = "I am learning";
+    sentance += " a lot of good stuff";
+
+    document.getElementById("Concatenate").innerHTML = sentance;
 }
