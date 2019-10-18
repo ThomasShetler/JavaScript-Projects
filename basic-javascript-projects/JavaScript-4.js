50fb5a985804d7c5c2ee574e5a58f285e89155ab
ThomasShetler <tommyaasum22@gmail.com>
2019-10-18 14:33:54 -0700

    HTML-4

---
+++ basic-javascript-projects/JavaScript-4.js
@@ -0,0 +1,7 @@
+function my_dictionary(){
+    var Animal = {
+    species:"dog",color:"black",Breed:"Labrador",Age:5, sound:"Bark!"       
+    };
+    document.getElementById("dictionary").innerHTML=Animal.sound;
+
+}
