b4fd375ec5ff05a237392c8ebc92c46ee2e8a070
ThomasShetler <tommyaasum22@gmail.com>
2019-10-20 14:42:23 -0700

    HTML 5

---
+++ basic-javascript-projects/javascript-5.js
@@ -0,0 +1,8 @@
+document.write(typeof 3);
+document.write(10 > 2);
+document.write(19+"null");
+console.log(2+2);
+document.write(5 > 2 && 10 > 4);
+function not_Function(){
+    document.getElementById("not").innerHTML=!(20>10);
+}
